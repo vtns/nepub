9f1ae399e983407e98dd55d2bc6989ebdfc8a9da
tama <tama@ttk1.net>
2022-07-23 19:12:34 +0900

    NarouParser の単体テスト追加

---
+++ test/test_parser.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from nepub.parser import NarouParser
+
+
+class TestNarouParser(TestCase):
+    def test_title_and_paragraphs(self):
+        parser = NarouParser()
+        parser.feed('''
+        <p class="novel_subtitle class2">title</p>
+        <p id="L1">aaaa</p>
+        <p id="L2">bbbb</p>
+        ''')
+        self.assertEqual('title', parser.title)
+        self.assertEqual(['aaaa', 'bbbb'], parser.paragraphs)
