4ef3dd75ca5eef70fd630325edd79378d5e255df
tama <tama@ttk1.net>
2022-07-23 18:16:52 +0900

    pipenv やめる + mypy 導入

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 from setuptools import setup, find_packages
 
-with open('requirements.txt', encoding='utf-8') as f:
-    install_requires = f.read()
+# with open('requirements.txt', encoding='utf-8') as f:
+#     install_requires = f.read()
 
 with open('README.md', encoding='utf-8') as f:
     readme = f.read()
@@ -26,7 +26,7 @@ setup(
     author_email='tama@ttk1.net',
     url='https://github.com/ttk1/nepub',
     # license=license,
-    install_requires=install_requires,
+    # install_requires=install_requires,
     packages=find_packages(exclude=('test',)),
     entry_points=entry_points
 )
